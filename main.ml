31d651a9349aa5acdabd187b95a10865179b34ff
mzp <mzpppp@gmail.com>
2010-01-09 09:33:16 +0900

    Stringモジュールから検索できるようにした

--- main.ml
+++ main.ml
@@ -1,4 +1,13 @@
 open Base
+open StdLabels
 
 let _ =
-  print_endline "hello"
+  let _ =
+    Toploop.set_paths () in
+  let _ =
+    Searchid.module_list := "String"::!Searchid.module_list
+  in
+  List.iter (Searchid.search_string_type Sys.argv.(1) `Included) ~f:begin
+    fun (id, kind) ->
+      print_endline @@ String.concat ~sep:"." @@ Longident.flatten id
+  end
