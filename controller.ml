b7a01d10a47070aa71a717e65936c3a3e84000ce
mzp <mzpppp@gmail.com>
2010-01-10 22:42:52 +0900

    不要なコンストラクタを削除

--- controller.ml
+++ controller.ml
@@ -4,7 +4,6 @@ open StdLabels
 type t =
     String of string
   | Bool   of bool
-  | Table  of (string * t) list
 
 let find_package module_ configs =
   let config =
--- controller.mli
+++ controller.mli
@@ -1,6 +1,5 @@
 type t =
     String of string
   | Bool   of bool
-  | Table  of (string * t) list
 
 val format : Config.t list -> Search.t -> (string * t) list
