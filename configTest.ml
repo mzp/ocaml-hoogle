f7a8467a09f0a4e6255f8161a6f9d45de6d5f1be
mzp <mzpppp@gmail.com>
2010-01-10 21:32:13 +0900

    configモジュールの単体テストを追加

---
+++ configTest.ml
@@ -0,0 +1,30 @@
+open Base
+open OUnit
+open Config
+
+let _ =
+  open_out_with "config-test.txt" begin fun ch ->
+    output_string ch "- some package\n";
+    output_string ch "A\n";
+    output_string ch "B\n";
+    output_string ch "- other package\n";
+    output_string ch "C\n"
+  end
+
+let _ = begin "config.ml" >::: [
+  "read" >:: begin fun () ->
+    assert_equal [
+      { name = "some package"; modules = ["A"; "B"]};
+      { name = "other package"; modules = ["C"]};
+    ]  @@
+      Config.read "config-test.txt"
+  end;
+  "find package" >:: begin fun () ->
+    assert_equal "other package" @@
+      find_package "C" [
+	{ name = "some package"; modules = ["A"; "B"]};
+	{ name = "other package"; modules = ["C"]};
+      ]
+  end
+] end +> run_test_tt_main
+
