18fc53547e0f5122fdce661566fe4c3efec78516
mzp <mzpppp@gmail.com>
2010-01-11 12:04:55 +0900

    page navigation を追加

--- main.ml
+++ main.ml
@@ -20,6 +20,27 @@ let rec to_var = function
 	end
       end
 
+let set t (name,var) =
+  match var with
+      Controller.String s ->
+	t#set name s
+    | Controller.Bool b ->
+	t#conditional name b
+    | Controller.Table rows ->
+	t#table name begin
+	  List.map rows ~f:begin
+	    List.map ~f:begin fun (name,var) ->
+	      (name,to_var var)
+	    end
+	  end
+	end
+
+let safe_int_of_string s =
+  try
+    int_of_string s
+  with _ ->
+    0
+
 let index_page (cgi : cgi) =
   cgi#template @@ template "templates/index.html"
 
@@ -27,15 +48,22 @@ let search_page (cgi : cgi) =
   let configs =
     Config.read "modules.txt"
   in
-  let result =
+  let page, content =
     HList.concat_map (fun {Config.modules=m} -> m) configs
     +> Search.search (cgi#param "q")
+    +> Controller.pagenation ~window:20 ~offset:(try
+						   int_of_string @@ cgi#param "o"
+						 with  _ -> 0)
+  in
+  let result =
+    content
     +> List.map ~f:(List.map ~f:(fun (name,v) -> (name,to_var v))
-		  $ Controller.format configs)
+		      $ Controller.format configs)
   in
   let t =
     template "templates/search.html"
   in
+    List.iter page ~f:(set t);
     t#set "query" @@ cgi#param "q";
     t#conditional "found" @@ (result <> []);
     t#table "result" result;
