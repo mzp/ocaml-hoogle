c6f26757b70717c8e80ccf637ab3ed43155f5276
Jun Furuse <jun.furuse@gmail.com>
2013-02-15 00:54:52 +0900

    removed debug print

--- search.ml
+++ search.ml
@@ -23,15 +23,15 @@ module Toploop = struct
   open Config
   open Misc
 
-let init_path () =
-  let dirs =
-    if !Clflags.use_threads then "+threads" :: !Clflags.include_dirs
-    else if !Clflags.use_vmthreads then "+vmthreads" :: !Clflags.include_dirs
-    else !Clflags.include_dirs in
-  let exp_dirs =
-    List.map (expand_directory Config.standard_library) dirs in
-  load_path := "" :: List.rev_append exp_dirs (Clflags.std_include_dir ());
-  Env.reset_cache ()
+  let init_path () =
+    let dirs =
+      if !Clflags.use_threads then "+threads" :: !Clflags.include_dirs
+      else if !Clflags.use_vmthreads then "+vmthreads" :: !Clflags.include_dirs
+      else !Clflags.include_dirs in
+    let exp_dirs =
+      List.map (expand_directory Config.standard_library) dirs in
+    load_path := "" :: List.rev_append exp_dirs (Clflags.std_include_dir ());
+    Env.reset_cache ()
 
   let set_paths () =
     init_path ();
@@ -41,13 +41,11 @@ let init_path () =
     load_path := !load_path @ [Filename.concat Config.standard_library "camlp4"];
     load_path := "" :: (List.rev !Clflags.include_dirs @ !load_path);
     Dll.add_path !load_path;
-List.iter (fun p -> prerr_endline ("XXX Toploop " ^ p)) !load_path
 end
 
 module Topdirs = struct
   open Misc
   let dir_directory s =
-prerr_endline ("XXX Topdirs " ^ s);
     let d = expand_directory Config.standard_library s in
     Config.load_path := d :: !Config.load_path;
     Dll.add_path [d]
@@ -57,8 +55,7 @@ let init modules paths =
   (* initialize *)
   Toploop.set_paths ();
   List.iter ~f:Topdirs.dir_directory paths;
-  Searchid.module_list := modules @ init_modules;
-List.iter (fun x -> prerr_endline ("XXX module " ^ x)) !Searchid.module_list
+  Searchid.module_list := modules @ init_modules
 
 let sure f x =
   try
@@ -170,9 +167,6 @@ let lift f s =
   +> List.map ~f:to_result
 
 let search s modules paths =
-  (* CR jfuruse: multiple calls of [search] accumulates load paths and others.
-     In CGI multiple searches cannot happen, but in test, possible.
-  *) 
   init modules paths; 
   List.rev @@
     ExtList.List.unique @@
